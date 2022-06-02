8b78de95393e0287e563b86ee717d98cce5f2288
0204-03 <106744838+0204-03@users.noreply.github.com>
2022-06-02 21:21:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
+  text1: "He luu cậu BPa^^! ",
+  text2: "Tớ có điều này muốn nói với cậu rằng.",
+  text3: "Cậu có bíc tớ yêu cậu vcl hông BPa?._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text5: "hông....hông có nhớ nữa hông...hông có bíc???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text7: "lí do mà tớ yêu cậu vcll là vì :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì cậu xinh đẹp nhứt trong lòng tớ BPa ạ =)))))",
+  text10: " ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "cho nên là BPa của tớ mãi xinh đẹp mãi hạnh phúc mặc dù nó sến vch ra BPa ạ :))))",
   text12: "Okii lunn <3",
 };
 
